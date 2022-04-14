1b90922da16f86d36e4dd3ccb76b4605e4768291
Brett Mastbergen <brettm@minim.com>
2022-04-14 16:09:06 -0400

    build_bcm_toolchain: Add deployment instructions
    After untaring the toolchain in its final location, you need to
    run sdk-relocate.sh to fix up all of the paths in the toolchain
    
    SW-1148

--- build_bcm_toolchain.sh
+++ build_bcm_toolchain.sh
@@ -4,5 +4,13 @@ make clean
 make broadcom_toolchain_defconfig
 export BR2_SDK_PREFIX=crosstools-arm-gcc-9.2-linux-4.19-glibc-2.34-binutils-2.32 ; make sdk -j32
 
+echo
+echo
+echo "Finished!!!"
+echo
 find output/images -name "*.tar.gz"
+echo
+echo "Untar ^^^ this in /opt/toolchains"
+echo "cd to /opt/toolchains/crosstools-arm-gcc..."
+echo "Then run ./relocate-sdk.sh"
 
