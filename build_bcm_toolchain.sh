820ec8f524e0bc63455668d88fdd7a52b029ba4c
Tom Keddie <github@bronwenandtom.com>
2023-05-30 10:38:55 -0700

    build_bcm_toolchain: bump gcc to 10.2 as per Broadcom SDK 5.04L04p1

--- build_bcm_toolchain.sh
+++ build_bcm_toolchain.sh
@@ -2,7 +2,7 @@
 
 make clean
 make broadcom_toolchain_defconfig
-export BR2_SDK_PREFIX=crosstools-arm-gcc-9.2-linux-4.19-glibc-2.34-binutils-2.32 ; make sdk -j32
+export BR2_SDK_PREFIX=crosstools-arm-gcc-10.2-linux-4.19-glibc-2.34-binutils-2.32 ; make sdk -j32
 
 echo
 echo
